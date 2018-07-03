540a0501cf56e711d78f4392aec3b2936a49006f
桥 吴 <wqq1991@live.com>
2018-07-03 16:58:34 +0800

    喂价器合约优化

--- OracleContract/OracleContract.cs
+++ OracleContract/OracleContract.cs
@@ -3,6 +3,7 @@ using Neo.SmartContract.Framework.Services.Neo;
 using Helper = Neo.SmartContract.Framework.Helper;
 using System;
 using System.Numerics;
+using Neo.SmartContract.Framework.Services.System;
 
 namespace OracleContract
 {
@@ -13,22 +14,46 @@ namespace OracleContract
 
         private const string CONFIG_NEO_PRICE = "neo_price";
         private const string CONFIG_SDT_PRICE = "sdt_price";
-          
+        private const string CONFIG_ACCOUNT = "account_key";
+
+
         public static Object Main(string operation, params object[] args)
-        { 
+        {
+            var callscript = ExecutionEngine.CallingScriptHash;
+
             var magicstr = "2018-07-02 15:16";
+
+            if (operation == "setAccount")
+            {
+                if (args.Length != 1) return false;
+
+                if (!Runtime.CheckWitness(admin)) return false;
+
+                byte[] account = (byte[])args[0];
+
+                Storage.Put(Storage.CurrentContext,CONFIG_ACCOUNT,account);
+
+                return true;
+            }
              
             if (operation == "setPrice")
             {
-                if (args.Length != 2) return false;
+                if (args.Length != 3) return false;
 
                 string key = (string)args[0];
 
-                BigInteger price = (BigInteger)args[1];
+                byte[] from = (byte[])args[1];
 
-               return setPrice(key, price);
+                byte[] account = Storage.Get(Storage.CurrentContext, CONFIG_ACCOUNT);
+                 
+                BigInteger price = (BigInteger)args[2];
+                 
+
+                if (callscript.AsBigInteger() != from.AsBigInteger() || (!Runtime.CheckWitness(from) || from != account)) return false;
+
+                  return setPrice(key, price);
             }
-
+             
             if (operation == "getPrice")
             {
                 if (args.Length != 2) return false;
@@ -39,16 +64,14 @@ namespace OracleContract
 
                 return getPrice(key);
             }
-            
+             
             return true;
         }
 
         public static bool setPrice(string key, BigInteger price)
         {
             if (key == null || key == "") return false;
-
-            if (!Runtime.CheckWitness(admin)) return false;
-            
+              
             Storage.Put(Storage.CurrentContext, key, price);
             return true;
         }
